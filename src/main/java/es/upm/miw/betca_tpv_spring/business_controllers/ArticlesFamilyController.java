cb455a7f6583cf109c472858f4831462fbad4f79
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-21 13:17:05 +0100

    refactor: remove comments and unused code #57

--- src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/ArticlesFamilyController.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/ArticlesFamilyController.java
@@ -6,14 +6,10 @@ import es.upm.miw.betca_tpv_spring.documents.FamilyComposite;
 import es.upm.miw.betca_tpv_spring.documents.FamilyType;
 import es.upm.miw.betca_tpv_spring.dtos.ArticleFamilyCompleteDto;
 import es.upm.miw.betca_tpv_spring.dtos.ArticlesFamilyDto;
-import es.upm.miw.betca_tpv_spring.dtos.FamilyCompositeDto;
 import es.upm.miw.betca_tpv_spring.repositories.ArticleRepository;
-import es.upm.miw.betca_tpv_spring.repositories.ArticlesFamilyReactRepository;
-import es.upm.miw.betca_tpv_spring.repositories.FamilyCompositeReactRepository;
 import es.upm.miw.betca_tpv_spring.repositories.FamilyCompositeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,31 +19,14 @@ import java.util.List;
 public class ArticlesFamilyController {
 
     @Autowired
-    private ArticlesFamilyReactRepository articlesFamilyReactRepository;
-
-    @Autowired
-    private FamilyCompositeReactRepository familyCompositeReactRepository;
-
-    @Autowired
     private FamilyCompositeRepository familyCompositeRepository;
 
     @Autowired
     private ArticleRepository articleRepository;
 
-//    public Mono<FamilyCompositeDto> readFamilyCompositeArticlesList(String description) {
-//        return this.familyCompositeReactRepository.findByReference(description)
-//                .map(FamilyCompositeDto::new);
-//    }
-
-
     public List<ArticleFamilyCompleteDto> readFamilyCompositeArticlesList(String description) {
         FamilyComposite familyComplete = familyCompositeRepository.findFirstByDescription(description);
         List<ArticleFamilyCompleteDto> dtos = new ArrayList<>();
-        System.out.println("description: " + description);
-        System.out.println("familyCompleteeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-        System.out.println(familyComplete);
-        System.out.println("articles family listttttt");
-        System.out.println(familyComplete.getArticlesFamilyList());
 
         if (familyComplete.getFamilyType() == FamilyType.ARTICLES) {
             for (ArticlesFamily articlesFamily : familyComplete.getArticlesFamilyList()) {
