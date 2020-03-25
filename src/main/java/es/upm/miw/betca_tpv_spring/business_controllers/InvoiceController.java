43bfe14d4e7fadcff272b15771a367f5b26e3353
jbernal <miw@etsisi.upm.es>
2020-03-25 11:08:24 +0100

    create invoice

--- src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/InvoiceController.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/InvoiceController.java
@@ -10,7 +10,6 @@ import es.upm.miw.betca_tpv_spring.repositories.TicketReactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -19,6 +18,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 public class InvoiceController {
@@ -41,56 +43,61 @@ public class InvoiceController {
 
     public Mono<Invoice> createInvoice() {
         Invoice invoice = new Invoice(0, null, null);
-        Mono<Ticket> ticket = ticketReactRepository.findFirstByOrderByCreationDateDescIdDesc()
+        Mono<Ticket> ticketPublisher = ticketReactRepository.findFirstByOrderByCreationDateDescIdDesc()
                 .switchIfEmpty(Mono.error(new NotFoundException("Last Ticket not found")))
-                .map(ticket1 -> {
+                .doOnNext(ticket1 -> {
                     invoice.setTicket(ticket1);
                     invoice.setUser(ticket1.getUser());
-                    return ticket1;
+                    System.out.println("==== ticket >>>> " + ticket1);
                 })
-                .handle((ticket1, synchronousSink) -> {
-                    User user = ticket1.getUser();
-                    if (ticket1.getUser() == null || !isUserCompletedForInvoice(user))
+                .handle((ticket, synchronousSink) -> {
+                    User user = ticket.getUser();
+                    if (ticket.getUser() == null || !isUserCompletedForInvoice(user))
                         synchronousSink.error(new BadRequestException("User not completed"));
-                    else if (ticket1.isDebt())
+                    else if (ticket.isDebt())
                         synchronousSink.error(new BadRequestException("Ticket is debt"));
                     else
-                        synchronousSink.complete();
+                        synchronousSink.next(ticket);
                 });
 
         Mono<Integer> nextId = this.nextIdStartingYearly()
-                .map(Id -> {
-                    invoice.setId(Id);
-                    return Id;
+                .map(id -> {
+                    invoice.setId(id);
+                    System.out.println("==== id >>>> " + id);
+                    return id;
                 });
-        return Mono.when(ticket, nextId).then(calculateBaseAndTax(invoice)).then(invoiceReactRepository.save(invoice));
+        Mono<Void> x = this.calculateBaseAndTax(invoice, ticketPublisher);
+        return Mono.when(x, nextId)
+                .then(invoiceReactRepository.save(invoice));
     }
 
-    private boolean isUserCompletedForInvoice(User user){
+
+    private boolean isUserCompletedForInvoice(User user) {
         return (user.getUsername() != null && !user.getUsername().trim().equals(""))
                 && (user.getAddress() != null && !user.getAddress().trim().equals(""))
                 && (user.getDni() != null && !user.getDni().trim().equals(""));
     }
 
-    private Mono<Invoice> calculateBaseAndTax(Invoice invoice) {
-        Flux<Article> articlesFlux = Flux.empty();
-        Shopping[] ticketShoppingList = Arrays.stream(invoice.getTicket().getShoppingList())
-                .filter(shopping -> shopping.getAmount() >= 1)
-                .toArray(Shopping[]::new);
-        for (Shopping shopping : ticketShoppingList) {
-            Mono<Article> articleReact = this.articleReactRepository.findById(shopping.getArticleId())
-                    .switchIfEmpty(Mono.error(new NotFoundException("Article(" + shopping.getArticleId() + ")")))
-                    .doOnNext(article -> {
-                        BigDecimal articleTaxRate = BigDecimal.ONE.divide(article.getTax().getRate(), RoundingMode.HALF_UP);
-                        BigDecimal articleTax = shopping.getShoppingTotal().multiply(articleTaxRate);
-                        invoice.setTax(invoice.getTax().add(articleTax));
-                        BigDecimal articleBaseTax = shopping.getShoppingTotal().subtract(articleTax);
-                        invoice.setBaseTax(invoice.getBaseTax().add(articleBaseTax));
-                    });
-            articlesFlux = Flux.merge(articleReact);
-        }
-        return articlesFlux.then(Mono.just(invoice));
+    private Mono<Void> calculateBaseAndTax(Invoice invoice, Mono<Ticket> ticketPublisher) {
+        return ticketPublisher
+                .switchIfEmpty(Mono.error(new NotFoundException("Ticket")))
+                .flatMap(ticket -> {
+                    Stream<Shopping> ticketShoppingList = Arrays.stream(invoice.getTicket().getShoppingList())
+                            .filter(shopping -> shopping.getAmount() >= 1);
+                    List<Mono<Article>> articlePublishers = ticketShoppingList
+                            .map(shopping -> this.articleReactRepository.findById(shopping.getArticleId())
+                                    .switchIfEmpty(Mono.error(new NotFoundException("Article(" + shopping.getArticleId() + ")")))
+                                    .doOnNext(article -> {
+                                        BigDecimal articleTaxRate = BigDecimal.ONE.divide(article.getTax().getRate(), RoundingMode.HALF_UP);
+                                        BigDecimal articleTax = shopping.getShoppingTotal().multiply(articleTaxRate);
+                                        invoice.setTax(invoice.getTax().add(articleTax));
+                                        BigDecimal articleBaseTax = shopping.getShoppingTotal().subtract(articleTax);
+                                        invoice.setBaseTax(invoice.getBaseTax().add(articleBaseTax));
+                                    })).collect(Collectors.toList());
+                    return Mono.when(articlePublishers);
+                });
     }
+
 
     private Mono<Integer> nextIdStartingYearly() {
         return invoiceReactRepository.findFirstByOrderByCreationDateDescIdDesc()
