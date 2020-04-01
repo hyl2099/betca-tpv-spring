d75581876923249f132f7e90671c52b9e0196816
rbn1993 <surfacerobin@outlook.com>
2020-04-01 22:22:29 +0200

    CustomerPointsResource completed #104

--- src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/CustomerPointsController.java
+++ src/main/java/es/upm/miw/betca_tpv_spring/business_controllers/CustomerPointsController.java
@@ -50,12 +50,12 @@ public class CustomerPointsController {
                 .switchIfEmpty(Mono.error(new NotFoundException("User mobile:" + mobile)));
     }
 
-    public Mono<String> createCustomerPointsByUserMobile(String mobile) {
+    public Mono<String> createCustomerPointsByExistingUserMobile(String mobile) {
         CustomerPoints customerPoints = new CustomerPoints();
         Mono<User> user = this.findUserByMobile(mobile).doOnNext(userDoc -> {
             customerPoints.setPoints(0);
             customerPoints.setUser(userDoc);
         });
-        return Mono.when(user).then(this.customerPointsReactRepository.save(customerPoints)).map(customerPoints1 -> customerPoints1.getUser().getMobile());
+        return Mono.when(user).then(this.customerPointsReactRepository.save(customerPoints)).map(newCustomerPoints -> newCustomerPoints.getUser().getMobile());
     }
 }
